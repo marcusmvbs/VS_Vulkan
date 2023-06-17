bab4cfb416abf7814db74a7ed3aed9f2728bf21c
marcusmvbs <marcus.mvbs@gmail.com>
2023-06-17 01:27:58 -0400

    rotating cube completed

--- src/app.cpp
+++ src/app.cpp
@@ -24,6 +24,7 @@ lveRenderer.getSwapChainRenderPass()};
   while (!lveWindow.shouldClose()) {
     glfwPollEvents();
     if (auto commandBuffer = lveRenderer.beginFrame()) {
+      
       lveRenderer.beginSwapChainRenderPass(commandBuffer);
       simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects);
       lveRenderer.endSwapChainRenderPass(commandBuffer);
@@ -33,21 +34,70 @@ lveRenderer.getSwapChainRenderPass()};
   vkDeviceWaitIdle(lveDevice.device());
 }
 
-void First_app::loadGameObjects() {
+std::unique_ptr<LveModel> createCubeModel(LveDevice& device, glm::vec3 offset) {
   std::vector<LveModel::Vertex> vertices{
-    {{0.0f, -0.5f}, {1.0f, 0.0f,  0.0f}}, 
-    {{0.5f, 0.5f}, {0.0f, 1.0f,  0.0f}},
-    {{-0.5f, 0.5f}, {0.0f, 0.0f,  1.0f}}};
-  auto lveModel = std::make_shared<LveModel>(lveDevice, vertices);
+   // left face (white)
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
 
-  auto triangle = LveGameObject::createGameObject();
-  triangle.model = lveModel;
-  triangle.color = {.99f, .4f, .0f};
-  triangle.transform2d.translation.x = .2f;
-  triangle.transform2d.scale = {2.f, .5f};
-  triangle.transform2d.rotation = .25f * glm::two_pi<float>();
+      // right face (yellow)
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
 
-  gameObjects.push_back(std::move(triangle));
+      // top face (orange, remember y axis points down)
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+
+      // bottom face (red)
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+
+      // nose face (blue)
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+
+      // tail face (green)
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}}
+
+  };
+  for (auto& v : vertices) {
+    v.position += offset;
+  }
+  return std::make_unique<LveModel>(device, vertices);
+}
+
+void First_app::loadGameObjects() {
+  std::shared_ptr<LveModel> lveModel = createCubeModel(lveDevice, {.0f, .0f, .0f});
+  auto cube = LveGameObject::createGameObject();
+  cube.model = lveModel;
+  cube.transform.translation = {.0f, .0f, .5f};
+  cube.transform.scale = {.5f, .5f, .5f};
+  gameObjects.push_back(std::move(cube));
 }
 
 }
